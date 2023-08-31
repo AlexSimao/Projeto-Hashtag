ef1210a1717baca4c6410d74927e5d15c118b6d2
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-08-31 00:43:23 -0300

    div-fechar-carrinho.
    Quando clicar fora do carrinho, o carrinho será fechado.

--- src/menu-carrinho.js
+++ src/menu-carrinho.js
@@ -1,15 +1,20 @@
 const carrinho = document.querySelector("#carrinho")
 carrinho.style.display = "none"
 
-const fechar_carrinho = document.querySelector("#btn-fechar-carrinho")
-const abrir_carrinho = document.querySelector("#btn-carrinho")
+const btn_fechar_carrinho = document.querySelector("#btn-fechar-carrinho")
+const btn_abrir_carrinho = document.querySelector("#btn-carrinho")
+const div_fechar = document.querySelector("#div-fechar-carrinho")
 
 const btnCarrinho = () => {  
     if (carrinho.style.display == "none"){
         carrinho.style.display = "block"
+        div_fechar.style.display = "block"
     } else if (carrinho.style.display == "block"){
         carrinho.style.display = "none"
+        div_fechar.style.display = "none"
     }
 }
-fechar_carrinho.addEventListener("click", btnCarrinho)
-abrir_carrinho.addEventListener("click", btnCarrinho)
+
+div_fechar.addEventListener("click", btnCarrinho)
+btn_fechar_carrinho.addEventListener("click", btnCarrinho)
+btn_abrir_carrinho.addEventListener("click", btnCarrinho)
