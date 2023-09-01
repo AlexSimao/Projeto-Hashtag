9baea21503f11d66bd6247c8fa72b76174b57d74
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-09-01 19:05:56 -0300

    Update card-produto.js

--- src/card-produto.js
+++ src/card-produto.js
@@ -1,35 +1,18 @@
 
 import { produto } from "./produtos.js";
 export const criarCard = () =>{
-        const container_produto = document.querySelector("#container-produto")
-        
-        for (let i in produto){
-            let card = document.createElement("div")
-            card.setAttribute("id", `card-produto-${produto[i].id}`)
-            card.setAttribute("class", "card-produto border-solid border-2 border-sky-500 w-48 box-shadow")
-            
-            let card_img = document.createElement("img")
-            card_img.setAttribute("src", produto[i].img)
-            
-            let card_marca = document.createElement("p")
-            card_marca.textContent = produto[i].marca
-            
-            let card_nome = document.createElement("p")
-            card_nome.textContent = produto[i].nome
-            
-            let card_preco = document.createElement("p")
-            card_preco.textContent = produto[i].preco.toLocaleString("pt-br", {style: "currency", currency: "BRL"})
-            
-            container_produto.appendChild(card)
-            card.appendChild(card_img)
-            card.appendChild(card_marca)
-            card.appendChild(card_nome)
-            card.appendChild(card_preco)
-            
-            let card_button = document.createElement("button")
-            card_button.textContent = "Adicionar"
-            card_button.setAttribute("id", `btn-add-produto-carrinho-${produto[i].id}`)
-            
-            card.appendChild(card_button)
-        }
+    const container_produto = document.querySelector("#container-produto")
+    
+    for (let i in produto){
+        container_produto.innerHTML += `
+        <div id="card-produto-${produto[i].id}" class="card-produto border-solid border-2 border-sky-500 w-48 box-shadow">
+            <img src="${produto[i].img}" alt="">
+
+            <p>${produto[i].marca}</p>
+            <p>${produto[i].nome}</p>
+            <p>${produto[i].preco.toLocaleString("pt-br", {style: "currency", currency: "BRL"})}</p>
+
+            <button id="btn-add-produto-carrinho-${produto[i].id}"><i class="fa-solid fa-cart-plus"></i></button>
+        </div>`
     }
+}
