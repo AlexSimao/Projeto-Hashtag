262831ac158c11cb63145c4893f2f4b53ab8a419
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-09-01 23:09:57 -0300

    Update card-produto.js

--- src/card-produto.js
+++ src/card-produto.js
@@ -1,18 +1,19 @@
 
-import { produto } from "./produtos.js";
+import { produtos } from "./produtos.js";
+import { addAoCarrinho } from "./menu-carrinho.js";
+
 export const criarCard = () =>{
-    const container_produto = document.querySelector("#container-produto")
     
-    for (let i in produto){
-        container_produto.innerHTML += `
-        <div id="card-produto-${produto[i].id}" class="card-produto border-solid border-2 border-sky-500 w-48 box-shadow">
-            <img src="${produto[i].img}" alt="">
+    for (let produto of produtos){
+        const cardProduto = `
+        <div id="card-produto-${produto.id}" class="card-produto w-48 box-shadow  flex flex-col p-2 justify-between group shadow-lg shadow-slate-500 rounded-lg overflow-hidden">
+            <img class="group-hover:scale-110 group-hover:shadow-lg group-hover:shadow-gray-300 duration-300 mb-4" src="${produto.img}" alt="Imagem do produto ${produto.nome}">
 
-            <p>${produto[i].marca}</p>
-            <p>${produto[i].nome}</p>
-            <p>${produto[i].preco.toLocaleString("pt-br", {style: "currency", currency: "BRL"})}</p>
+            <p class="text-sm">${produto.marca}</p>
+            <p class="text-sm">${produto.nome}</p>
+            <p>${produto.preco.toLocaleString("pt-br", {style: "currency", currency: "BRL"})}</p>
 
-            <button id="btn-add-produto-carrinho-${produto[i].id}"><i class="fa-solid fa-cart-plus"></i></button>
+            <button id="btn-add-produto-${produto.id}" class="bg-emerald-950 text-white active:text-stone-400 hover:bg-emerald-900"><i class="fa-solid fa-cart-plus"></i></button>
         </div>`
     }
 }
