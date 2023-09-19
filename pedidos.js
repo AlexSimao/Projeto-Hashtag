3bc45518c5e830c3646a8a381f885b0ab2974f49
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-09-18 22:29:09 -0300

    Criando a funçao criarCardsDoHistoricoDePedidos()

--- pedidos.js
+++ pedidos.js
@@ -3,3 +3,21 @@ import {
   lerLocalStorage,
 } from "./src/utilidades";
 
+export function criarCardsDoHistoricoDePedidos(pedidoComData) {
+  const elementoPedido = `
+    <p class="text-lg font-bold">${pedidoComData.dataDoPedido}</p>
+    <section id="container-pedido-${pedidoComData.dataDoPedido}">
+        
+    </section>
+    `;
+  const main = document.querySelector("main");
+  main.innerHTML += elementoPedido;
+
+  for (const idProduto in pedidoComData.itensPedidos) {
+    desenharProdutoAoCarrinhoSimples(
+      idProduto,
+      `container-pedido-${pedidoComData.dataDoPedido}`,
+      pedidoComData.itensPedidos[idProduto]
+    );
+  }
+}
