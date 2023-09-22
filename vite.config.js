34e7561cb956ac66a4e91588c15c5be3d5513c83
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-09-22 19:30:05 -0300

    Editando o vite.config.js e criando a pasta dist.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,25 @@
+import tailwindcss from "tailwindcss";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
-export default defineConfig ({
-    base: "/"
-})
+export default defineConfig({
+  base: "/Projeto-Hashtag/",
+  plugins: [],
+  resolve: {
+    /* something */
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./index.html"),
+        checkout: resolve(__dirname, "./checkout.html"),
+        pedidos: resolve(__dirname, "./pedidos.html"),
+      },
+    },
+  },
+});
